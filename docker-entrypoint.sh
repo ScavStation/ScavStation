113187d062dac5be65cf38aa7f235e0aa6921f74
MistakeNot4892 <AGMogett@gmail.com>
2020-02-18 17:31:35 +1100

    Revert "Renamed args/repo stuff to Nebula."
    This reverts commit d3bc805c15a1c2c1c910dda86a2e3eca14e6afa1.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir /byond
-chown $RUNAS:$RUNAS /byond /nebula nebula.rsc
+chown $RUNAS:$RUNAS /byond /scav scavstation.rsc
 
-gosu $RUNAS DreamDaemon nebula.dmb 8000 -trusted -verbose
+gosu $RUNAS DreamDaemon scavstation.dmb 8000 -trusted -verbose
